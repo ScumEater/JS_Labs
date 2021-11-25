3dbf6ab3a52f3ad30d85e5021514a140871eb641
Scumeater <nikita2013282@gmail.com>
2021-11-25 20:18:05 +0300

    Правки по заданию))

--- js/app.js
+++ js/app.js
@@ -2,22 +2,18 @@ import Rectangle from './rectangle.js';
 
 class Application {
     constructor() {
-        this.canvas = document.getElementById('canvas')
+        this.canvas = document.getElementById('canvas');
         this.ctx = canvas.getContext('2d');
-        this.canvas.width = 600;
-        this.canvas.height = 600;
-        this.arr = [];
     }
-
     generateRectangles() {
-        for (let squareCnt = 0; squareCnt < 25; squareCnt++) {
-            let rectangle = new Rectangle(squareCnt);
-            let rectangle_draw = new Rectangle(this.ctx);
-            this.arr.push(rectangle);
-            rectangle_draw.draw();
-            console.log(rectangle);
+        let rectangle = new Rectangle();
+        rectangle.draw();
         }
-    }
+
 }
-const app = new Application()
+
+const app = new Application();
 app.generateRectangles();
+
+
+
--- js/rectangle.js
+++ js/rectangle.js
@@ -1,17 +1,28 @@
 class Rectangle {
-    constructor(ctx) {
-        this.ctx = ctx;
-        this.width = 1 + (Math.random() % 100 + 50);;
-        this.height = 1 + (Math.random() % 100 + 50);
-        this.color = "#" + Math.round(Math.random() * 16777215).toString(16);
+    constructor() {
+        this.ctx = canvas.getContext('2d');
+        this.cw = canvas.width;
+        this.ch = canvas.height;
+        this.cols = 5;
+        this.rows = 5;
+        this.padding = 5;
+        this.w = (this.cw - this.padding * this.cols) / this.cols;
+        this.h = (this.ch - this.padding * this.rows) / this.rows;
+        this.colors = [];
     }
-
 
     draw() {
-        this.ctx.save();
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect((this.width / 2),(this.height / 2),250,250);
-        this.ctx.restore();
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                this.ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);;
+                this.ctx.fillRect(x * (this.w + this.padding), y * (this.h + this.padding), this.w, this.h);
+            }
+        }
+    }
+
+    randomColor() {
+        return ('#' + Math.floor(Math.random() * 16777215).toString(16));
     }
 }
+
 export default Rectangle;
