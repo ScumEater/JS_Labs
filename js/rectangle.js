1afcfe42bfec4c9cb86cb631af5f67a54e2992ea
Scumeater <nikita2013282@gmail.com>
2021-11-25 21:19:11 +0300

    Обновление rectangle.js

---
+++ js/rectangle.js
@@ -0,0 +1,28 @@
+class Rectangle {
+    constructor() {
+        this.ctx = canvas.getContext('2d');
+        this.cw = canvas.width;
+        this.ch = canvas.height;
+        this.cols = 5;
+        this.rows = 5;
+        this.padding = 5;
+        this.w = (this.cw - this.padding * this.cols) / this.cols;
+        this.h = (this.ch - this.padding * this.rows) / this.rows;
+        this.colors = [];
+    }
+
+    draw() {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                this.ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);;
+                this.ctx.fillRect(x * (this.w + this.padding), y * (this.h + this.padding), this.w, this.h);
+            }
+        }
+    }
+
+    randomColor() {
+        return ('#' + Math.floor(Math.random() * 16777215).toString(16));
+    }
+}
+
+export default Rectangle;
