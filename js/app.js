3d1d8ebaa6574a11bfc115de9e0704c289de6a2d
Scumeater <nikita2013282@gmail.com>
2021-11-25 21:16:44 +0300

    Обновление app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("test");
+import Rectangle from './rectangle.js';
+
+class Application {
+    constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = canvas.getContext('2d');
+    }
+    generateRectangles() {
+        let rectangle = new Rectangle();
+        rectangle.draw();
+    }
+
+}
+
+const app = new Application();
+app.generateRectangles();
