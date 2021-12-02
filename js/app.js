bb593eaae5a562170048dc3459472d6d6bafff3c
Scumeater <nikita2013282@gmail.com>
2021-12-02 12:44:52 +0300

    Commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
-import Rectangle from './rectangle.js';
+import Tile from './tile.js';
+import FPS from './fps.js';
 
 class Application {
     constructor() {
         this.canvas = document.getElementById('canvas');
         this.ctx = canvas.getContext('2d');
     }
-    generateRectangles() {
-        let rectangle = new Rectangle();
-        rectangle.draw();
-    }
+    Run() {
+        let tile = new Tile(this.ctx);
+        tile.draw();
 
+    }
+}
+const app = new Application();
+
+
+
+requestAnimationFrame(Animation);
+
+function Animation() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    app.Run();
+    requestAnimationFrame(Animation);
 }
 
+var fps = new FPS();
+fps.start();
 
-const app = new Application();
-app.generateRectangles();
